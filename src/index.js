37f65686112927b14fa652d779f4e4d998045cfb
ktapia576 <ktapia576@hotmail.com>
2020-10-04 01:22:34 -0400

    Add states to components and keep track of sqaure state in Board component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button 
+                className="square"
+                onClick={() =>  this.props.onClick()}
+            >
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return ( 
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
@@ -20,19 +40,19 @@ class Board extends React.Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
                 </div>
                 <div className="board-row">
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
                 </div>
                 <div className="board-row">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
         );
@@ -44,11 +64,11 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                <Board />
+                    <Board />
                 </div>
                 <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
                 </div>
             </div>
         );
